e2aca1f1b0dc4daf4bfbfa2d2341967c414e6086
jovaandres <jovaandrea8721@gmail.com>
2021-11-09 18:17:58 +0700

    support cmyk, l, la, p, rgb, rgba

--- src/backend/api.py
+++ src/backend/api.py
@@ -1,11 +1,6 @@
 from flask import Blueprint, request
 from flask_restx import Api, Resource
-from PIL import Image
-from io import BytesIO
-import base64
-from img_process import img_to_matrix, matrix_to_img, compress_matrix, svd_to_matrix
-from numpy.linalg import svd
-from numpy import diag
+from img_process import compress_image
 from time import time
 
 api_blueprint = Blueprint('api', __name__, url_prefix='/api')
@@ -27,32 +22,10 @@ class Test(Resource):
         image_mime = image_body[0]
         image_base64 = image_body[1]
 
-        image = base64.b64decode(str(image_base64))
-        image = Image.open(BytesIO(image))
-
         t1 = time()
-        r, g, b = img_to_matrix(image)
-
-        ru,rs,rv=svd(r)
-        gu,gs,gv=svd(g)
-        bu,bs,bv=svd(b)
-
-        new_r = compress_matrix([ru, diag(rs), rv], compress_ratio)
-        new_g = compress_matrix([gu, diag(gs), gv], compress_ratio)
-        new_b = compress_matrix([bu, diag(bs), bv], compress_ratio)
-
-        new_r = svd_to_matrix(new_r)
-        new_g = svd_to_matrix(new_g)
-        new_b = svd_to_matrix(new_b)
-
-        buffer = [new_r, new_g, new_b]
-
-        buffer, img_type = matrix_to_img(buffer)
+        img_str = compress_image(image_base64, compress_ratio)
         t2 = time()
 
-        bytes_buffer = BytesIO()
-        buffer.save(bytes_buffer, img_type)
-        img_str = base64.b64encode(bytes_buffer.getvalue())
         return {
             'image': image_mime + "," + img_str.decode("utf-8"),
             'time': round(t2 - t1, 2)
--- src/backend/img_process.py
+++ src/backend/img_process.py
@@ -2,14 +2,30 @@ from PIL import Image
 from numpy import asarray, ma
 from numpy import uint8
 from numpy.lib.shape_base import dstack
+from numpy.lib.twodim_base import diag
+from numpy.linalg import svd
+from base64 import b64decode, b64encode
+from io import BytesIO
 
-def img_to_matrix(image):
+
+def img_to_matrix(image, mode):
     imageAr = asarray(image)
-    imgType = len(imageAr[0][0])
+
+    if mode == "L" or mode == "P":
+        return [imageAr]
+    if mode == "LA":
+        color = 2
+    elif mode == "RGB":
+        color = 3
+    elif mode == "RGBA" or mode == "CMYK":
+        color = 4
+
     buffer = []
-    for i in range(imgType):
-        buffer.append(imageAr[:,:,i])
+    for i in range(color):
+        buffer.append(imageAr[:, :, i])
+
     return buffer
+
 
 def compress_matrix(matrix_svd, diff):
     rank = len(matrix_svd[1][0])
@@ -19,28 +35,71 @@ def compress_matrix(matrix_svd, diff):
     new_svd = [u[:, :new_rank], e[:new_rank, :new_rank], vt[:new_rank, :]]
     return new_svd
 
+
 def svd_to_matrix(matrix_svd):
     u, e, vt = matrix_svd
     return u @ e @ vt
 
-def matrix_to_img(buffer):
-    buffer_length = len(buffer)
-    if buffer_length == 3:
-        img_type = "JPEG"
-    elif buffer_length == 4:
-        img_type = "PNG"
 
-    return Image.fromarray(uint8(dstack(buffer))), img_type
+def matrix_to_img(buffer, convert=None):
+    if len(buffer) > 1:
+        return Image.fromarray(uint8(dstack(buffer))).convert(convert)
+    else:
+        return Image.fromarray(uint8(buffer[0])).convert(convert)
+
 
 def mask_png(buffer, mask):
     newBuffer = []
     for i in range(len(buffer)):
-        mask_array = mask[i]                
+        mask_array = mask[i]
         png_arr = buffer[i]
 
-        m = ma.masked_where(mask_array==0, mask_array, copy=False)
+        m = ma.masked_where(mask_array == 0, mask_array, copy=False)
         png_arr = ma.masked_where(ma.getmask(m), png_arr, copy=False)
         png_arr = png_arr.filled(0)
         png_arr = png_arr.astype(uint8)
         newBuffer.append(png_arr)
     return newBuffer
+
+
+def compress_image(b64Img: str, ratio: int):
+    image = b64decode(b64Img)
+    image = Image.open(BytesIO(image))
+    original_mode = image.mode
+    if original_mode == "P":
+        image = image.convert("RGBA")
+        mode = image.mode
+    elif original_mode == "CMYK":
+        image = image.convert("RGB")
+        mode = image.mode
+    else:
+        mode = original_mode
+
+    buffer = img_to_matrix(image, mode)
+    new_buffer = []
+
+    if mode == "RGB" or mode == "L":
+        print("AA")
+        img_type = "JPEG"
+        for color in buffer:
+            u, s, vt = svd(color)
+            new_color = compress_matrix([u, diag(s), vt], ratio)
+            new_color = svd_to_matrix(new_color)
+            new_buffer.append(new_color)
+        img = matrix_to_img(new_buffer, original_mode)
+
+    elif mode == "RGBA" or mode == "LA":
+        img_type = "PNG"
+        for color in buffer[:-1]:
+            u, s, vt = svd(color)
+            new_color = compress_matrix([u, diag(s), vt], ratio)
+            new_color = svd_to_matrix(new_color)
+            new_buffer.append(new_color)
+        new_buffer = mask_png(new_buffer, buffer[:-1])
+        new_buffer.append(buffer[-1])
+        img = matrix_to_img(new_buffer, original_mode)
+
+    image_buffer = BytesIO()
+    img.save(image_buffer, format=img_type)
+    img_str = b64encode(image_buffer.getvalue())
+    return img_str
